4180de12d2a3149a3e78bc18121fafd9e8e02269
Aqua920303 <aqua920303@gmail.com>
2025-04-10 14:32:35 +0900

    vercel.json 추가로 SPA 라우팅 문제 해결

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  publicDir: 'public',
-  build: {
-    rollupOptions: {
-      input: './public/index.html'
-    }
-  }
-});
+  root: '.',
+})
